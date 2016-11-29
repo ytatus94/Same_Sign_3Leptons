f6fc40daeb6b7b2ec648df4835c8b1c8e0838f3f
ytshen <ytshen@MacBookPro-2.local>
2016-11-30 00:01:40 +0100

    Change space

--- README.md
+++ README.md
@@ -1,32 +1,36 @@
 # SS_v37
 This is SS/3L+jet cutflows study.
 
-4topSM v37 Sample: user.jpoveda.t0756_v37.410080.MadGraphPythia8EvtGen_A14NNPDF23_4topSM.DAOD_SUSY2.s2608_r7725_p2613_output.root
+Samples:
+* 4topSM v37 Sample: `user.jpoveda.t0756_v37.410080.MadGraphPythia8EvtGen_A14NNPDF23_4topSM.DAOD_SUSY2.s2608_r7725_p2613_output.root`
+* ttbar v37 Sample: `user.jpoveda.t0756_v37.410000.PowhegPythiaEvtGen_P2012_ttbar_hdamp17    2p5_nonallhad.DAOD_SUSY2.s2608_r7725_p2613_output.root`
 
-ttbar v37 Sample: user.jpoveda.t0756_v37.410000.PowhegPythiaEvtGen_P2012_ttbar_hdamp17    2p5_nonallhad.DAOD_SUSY2.s2608_r7725_p2613_output.root
+### v08:
+Change the descriptions in `README.md`
 
-v08: Change the descriptions in README.md
+### v07:
+Add ttbar samples into `util/cutflow.cxx`
 
-v07: Add ttbar samples into util/cutflow.cxx
+### v06:
+Modify the `yt_cutflows.h` and `yt_selector.h`
 
-v06: Modify the yt_cutflows.h and yt_selector.h
-1. Add degug_print functions.
-2. Correct the prototypes of pass_at_least_one_bjet_greater_than_20GeV() and pass_four_jets_greater_than_50GeV() functions.
+1. Add `degug_print()` functions.
+2. Correct the prototypes of `pass_at_least_one_bjet_greater_than_20GeV()` and `pass_four_jets_greater_than_50GeV()` functions.
 
 
-v05: comment out some output.
+### v05: Comment out some output.
 
-v04: Add EventNumber protection.
-1. Using the passOR vectors to fill the signal vectors.
-2. Adding vec_event_number to save EventNumber. Use this mechanism to protect the channel selection part. All the events used in channel selection must be the events pass same-sign cut.
+### v04: Add EventNumber protection.
+1. Using the `passOR` vectors to fill the signal vectors.
+2. Adding `vec_event_number` to save `EventNumber`. Use this mechanism to protect the channel selection part. All the events used in channel selection must be the events pass same-sign cut.
 
-v03: Add channel selection
+### v03: Add channel selection
 Adding codes for channel selection. As long as there are at lease two signal leptons with pT>20 GeV, we don’t require the pT of same sign pair greater than 20 GeV.
 
 
-v02: Make RootCore package structure.
+### v02: Make RootCore package structure.
 Creating folders and moving files to the corresponding folders such that the structure satisfies the RootCore requirement.
 
 
-v01: First commit
+### v01: First commit
 The cutflow works to same sign cut.
--- util/cutflow.cxx
+++ util/cutflow.cxx
@@ -10,66 +10,66 @@ using namespace std;
 
 int main( int argc, char* argv[] )
 {
-	gROOT->Reset();
-	gInterpreter->EnableAutoLoading();
+    gROOT->Reset();
+    gInterpreter->EnableAutoLoading();
 
-	// The application's name:
-	const char* APP_NAME = argv[0];
+    // The application's name:
+    const char* APP_NAME = argv[0];
 
-	if (argc < 2) {
-		Error(APP_NAME, " Usage: %s [isData=0/1 isMC=0/1]", APP_NAME);
-		exit(0);
-	}
+    if (argc < 2) {
+        Error(APP_NAME, " Usage: %s [isData=0/1 isMC=0/1]", APP_NAME);
+        exit(0);
+    }
 
-	int isData = 0;
-	int isMC   = 0;
-	for (int i = 1; i < argc; i++) {
-		const char* key = strtok(argv[i], "=");
-		const char* val = strtok(0, " ");
-		if (strcmp(key, "isData") == 0 ) isData = atoi(val);
-		if (strcmp(key, "isMC") == 0 ) isMC = atoi(val);
-	}
+    int isData = 0;
+    int isMC   = 0;
+    for (int i = 1; i < argc; i++) {
+        const char* key = strtok(argv[i], "=");
+        const char* val = strtok(0, " ");
+        if (strcmp(key, "isData") == 0 ) isData = atoi(val);
+        if (strcmp(key, "isMC") == 0 ) isMC = atoi(val);
+    }
 
-	Info(APP_NAME, "isMC = %i, isData = %i", isMC, isData);
+    Info(APP_NAME, "isMC = %i, isData = %i", isMC, isData);
 
-	TChain *fChain = new TChain("AnaNtup", "chain");
+    TChain *fChain = new TChain("AnaNtup", "chain");
 
-	// for data
-	if (isData) {
-		cout << "Add data files to TChain..." << endl;
-		cout << "Currently, no data." << endl;
-		//fChain->Add("../user.jpoveda.t0609_v12.00266904.physics_Main.DAOD_SUSY9.f594_m1435_p2361_output.root/user.jpoveda.5632900._000001.output.root");
-	}
-	// for MC
-	if (isMC) {
-		cout << "Add MC files to TChain..." << endl;
-		TString path = "/afs/cern.ch/work/y/yushen/private/Ximo/v37/ytCutflows/data/user.jpoveda.t0756_v37.410080.MadGraphPythia8EvtGen_A14NNPDF23_4topSM.DAOD_SUSY2.s2608_r7725_p2613_output.root";
-		fChain->Add(path + "/user.jpoveda.8482167._000001.output.root");
+    // for data
+    if (isData) {
+        cout << "Add data files to TChain..." << endl;
+        cout << "Currently, no data." << endl;
+        //fChain->Add("../user.jpoveda.t0609_v12.00266904.physics_Main.DAOD_SUSY9.f594_m1435_p2361_output.root/user.jpoveda.5632900._000001.output.root");
+    }
+    // for MC
+    if (isMC) {
+        cout << "Add MC files to TChain..." << endl;
+        TString path = "/afs/cern.ch/work/y/yushen/private/Ximo/v37/ytCutflows/data/user.jpoveda.t0756_v37.410080.MadGraphPythia8EvtGen_A14NNPDF23_4topSM.DAOD_SUSY2.s2608_r7725_p2613_output.root";
+        fChain->Add(path + "/user.jpoveda.8482167._000001.output.root");
 /*
-		TString path = "/afs/cern.ch/work/y/yushen/private/Ximo/v37/ytCutflows/data/user.jpoveda.t0756_v37.410000.PowhegPythiaEvtGen_P2012_ttbar_hdamp172p5_nonallhad.DAOD_SUSY2.s2608_r7725_p2613_output.root";
-		fChain->Add(path + "/user.jpoveda.8481848._000001.output.root");
-		fChain->Add(path + "/user.jpoveda.8481848._000002.output.root");
-		fChain->Add(path + "/user.jpoveda.8481848._000003.output.root");
-		fChain->Add(path + "/user.jpoveda.8481848._000004.output.root");
-		fChain->Add(path + "/user.jpoveda.8481848._000005.output.root");
-		fChain->Add(path + "/user.jpoveda.8481848._000006.output.root");
-		fChain->Add(path + "/user.jpoveda.8481848._000007.output.root");
-		fChain->Add(path + "/user.jpoveda.8481848._000008.output.root");
-		fChain->Add(path + "/user.jpoveda.8481848._000009.output.root");
-		fChain->Add(path + "/user.jpoveda.8481848._000010.output.root");
-		fChain->Add(path + "/user.jpoveda.8481848._000011.output.root");
-		fChain->Add(path + "/user.jpoveda.8481848._000012.output.root");
-		fChain->Add(path + "/user.jpoveda.8481848._000013.output.root");
-		fChain->Add(path + "/user.jpoveda.8481848._000014.output.root");
-		fChain->Add(path + "/user.jpoveda.8481848._000015.output.root");
-		fChain->Add(path + "/user.jpoveda.8481848._000016.output.root");
-		fChain->Add(path + "/user.jpoveda.8481848._000017.output.root");
-		fChain->Add(path + "/user.jpoveda.8481848._000018.output.root");
+        TString path = "/afs/cern.ch/work/y/yushen/private/Ximo/v37/ytCutflows/data/user.jpoveda.t0756_v37.410000.PowhegPythiaEvtGen_P2012_ttbar_hdamp172p5_nonallhad.DAOD_SUSY2.s2608_r7725_p2613_output.root";
+        fChain->Add(path + "/user.jpoveda.8481848._000001.output.root");
+        fChain->Add(path + "/user.jpoveda.8481848._000002.output.root");
+        fChain->Add(path + "/user.jpoveda.8481848._000003.output.root");
+        fChain->Add(path + "/user.jpoveda.8481848._000004.output.root");
+        fChain->Add(path + "/user.jpoveda.8481848._000005.output.root");
+        fChain->Add(path + "/user.jpoveda.8481848._000006.output.root");
+        fChain->Add(path + "/user.jpoveda.8481848._000007.output.root");
+        fChain->Add(path + "/user.jpoveda.8481848._000008.output.root");
+        fChain->Add(path + "/user.jpoveda.8481848._000009.output.root");
+        fChain->Add(path + "/user.jpoveda.8481848._000010.output.root");
+        fChain->Add(path + "/user.jpoveda.8481848._000011.output.root");
+        fChain->Add(path + "/user.jpoveda.8481848._000012.output.root");
+        fChain->Add(path + "/user.jpoveda.8481848._000013.output.root");
+        fChain->Add(path + "/user.jpoveda.8481848._000014.output.root");
+        fChain->Add(path + "/user.jpoveda.8481848._000015.output.root");
+        fChain->Add(path + "/user.jpoveda.8481848._000016.output.root");
+        fChain->Add(path + "/user.jpoveda.8481848._000017.output.root");
+        fChain->Add(path + "/user.jpoveda.8481848._000018.output.root");
 */
-	}
+    }
 
-	yt_selector *foo = new yt_selector;
-	foo->isMC = isMC;
-	foo->isData = isData;
-	fChain->Process(foo);
+    yt_selector *foo = new yt_selector;
+    foo->isMC = isMC;
+    foo->isData = isData;
+    fChain->Process(foo);
 }
