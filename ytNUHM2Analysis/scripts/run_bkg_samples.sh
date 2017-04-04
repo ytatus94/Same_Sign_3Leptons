cf53d97574be0980885b9a86ddcb4118c35f0eb9
ytshen <ytatus94@yahoo.com.tw>
2017-04-04 12:16:40 +0200

    add two scripts

---
+++ ytNUHM2Analysis/scripts/run_bkg_samples.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+Run optimization isMC ttW > optimization_MC_ttW.log 2>&1 &
+Run optimization isMC ttee > optimization_MC_ttee.log 2>&1 &
+Run optimization isMC ttmumu > optimization_MC_ttmumu.log 2>&1 &
+Run optimization isMC tttautau > optimization_MC_tttautau.log 2>&1 &
+
+Run optimization isMC llvvjj_ss_EW4 > optimization_MC_llvvjj_ss_EW4.log 2>&1 &
+Run optimization isMC llvvjj_ss_EW6 > optimization_MC_llvvjj_ss_EW6.log 2>&1 &
+Run optimization isMC lllvjj_EW6 > optimization_MC_lllvjj_EW6.log 2>&1 &
+Run optimization isMC lllljj_EW6 > optimization_MC_lllljj_EW6.log 2>&1 &
+Run optimization isMC ggllll > optimization_MC_ggllll.log 2>&1 &
+Run optimization isMC NNPDF30NNLO_llll > optimization_MC_llll.log 2>&1 &
+Run optimization isMC NNPDF30NNLO_lllv > optimization_MC_lllv.log 2>&1 &
+
+Run optimization isMC 3top_SM > optimization_MC_3top_SM.log 2>&1 &
+Run optimization isMC WH125_inc > optimization_MC_WH125_inc.log 2>&1 &
+Run optimization isMC ZH125_inc > optimization_MC_ZH125_inc.log 2>&1 &
+Run optimization isMC ttH125_di > optimization_MC_ttH125_di.log 2>&1 &
+Run optimization isMC ttH125_se > optimization_MC_ttH125_se.log 2>&1 &
+Run optimization isMC ttH125_al > optimization_MC_ttH125_al.log 2>&1 &
+Run optimization isMC WWW_3l3v > optimization_MC_WWW_3l3v.log 2>&1 &
+Run optimization isMC WWZ_4l2v > optimization_MC_WWZ_4l2v.log 2>&1 &
+Run optimization isMC WWZ_2l4v > optimization_MC_WWZ_2l4v.log 2>&1 &
+Run optimization isMC WZZ_5l1v > optimization_MC_WZZ_5l1v.log 2>&1 &
+Run optimization isMC WZZ_3l3v > optimization_MC_WZZ_3l3v.log 2>&1 &
+Run optimization isMC ZZZ_6l0v > optimization_MC_ZZZ_6l0v.log 2>&1 &
+Run optimization isMC ZZZ_4l2v > optimization_MC_ZZZ_4l2v.log 2>&1 &
+Run optimization isMC ZZZ_2l4v > optimization_MC_ZZZ_2l4v.log 2>&1 &
+Run optimization isMC tZ_4fl_tchan_noAllHad > optimization_MC_tZ_4fl_tchan_noAllHad.log 2>&1 &
+Run optimization isMC 4topSM > optimization_MC_4topSM.log 2>&1 &
+Run optimization isMC ttbarWW > optimization_MC_ttbarWW.log 2>&1 &
+Run optimization isMC tWZDR > optimization_MC_tWZDR.log 2>&1 &
+
---
+++ ytNUHM2Analysis/scripts/run_signal_samples.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+Run optimization isMC NUHM2_m12_300_strong > optimization_NUHM2_m12_300_strong.log 2>&1 &
+Run optimization isMC NUHM2_m12_350_strong > optimization_NUHM2_m12_350_strong.log 2>&1 &
+Run optimization isMC NUHM2_m12_400_strong > optimization_NUHM2_m12_400_strong.log 2>&1 &
+Run optimization isMC NUHM2_m12_500_strong > optimization_NUHM2_m12_500_strong.log 2>&1 &
+Run optimization isMC NUHM2_m12_600_strong > optimization_NUHM2_m12_600_strong.log 2>&1 &
+Run optimization isMC NUHM2_m12_700_strong > optimization_NUHM2_m12_700_strong.log 2>&1 &
+Run optimization isMC NUHM2_m12_800_strong > optimization_NUHM2_m12_800_strong.log 2>&1 &
+# Run optimization isMC NUHM2_m12_300_weak > NUHM2_m12_300_weak.log 2>&1 &
+# Run optimization isMC NUHM2_m12_350_weak > NUHM2_m12_350_weak.log 2>&1 &
+# Run optimization isMC NUHM2_m12_400_weak > NUHM2_m12_400_weak.log 2>&1 &
+# Run optimization isMC NUHM2_m12_500_weak > NUHM2_m12_500_weak.log 2>&1 &
+# Run optimization isMC NUHM2_m12_600_weak > NUHM2_m12_600_weak.log 2>&1 &
+# Run optimization isMC NUHM2_m12_700_weak > NUHM2_m12_700_weak.log 2>&1 &
+# Run optimization isMC NUHM2_m12_800_weak > NUHM2_m12_800_weak.log 2>&1 &
